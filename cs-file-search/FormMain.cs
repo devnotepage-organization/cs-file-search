0a8421ab18b4c16b95a60ef1fb04303b64fc12af
krmgst <krmgst@outlook.com>
2024-01-28 20:11:14 +0900

    Update

--- cs-file-search/FormMain.cs
+++ cs-file-search/FormMain.cs
@@ -14,20 +14,56 @@ namespace cs_file_search
 {
     public partial class FormMain : Form
     {
+        private IEnumerable<string> _searchPathList;
+        private Dictionary<string, int> _fileList;
         public FormMain()
         {
             InitializeComponent();
             LoadConfig();
+            LoadFileList();
         }
         public void LoadConfig()
         {
             // JSONファイル読み込み
-            string jsonString = System.IO.File.ReadAllText("../../config.json");
+            string jsonString = string.Empty;
+            string configPath = "config.json";
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    jsonString = System.IO.File.ReadAllText(configPath);
+                    // ok
+                    break;
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine("error:" + e.Message);
+                    // retry
+                    configPath = "../" + configPath;
+                }
+            }
 
+            // 検索パスリスト取得
             JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
             JsonElement root = jsonDocument.RootElement;
-            var searchPath = root.GetProperty("searchPath").EnumerateArray();
-            System.Console.WriteLine(searchPath.ToString());
+            var searchPathList = root.GetProperty("searchPath").EnumerateArray();
+            _searchPathList = searchPathList.Select(x => x.ToString());
+        }
+        public void LoadFileList()
+        {
+            string fileListString = string.Empty;
+            try
+            {
+                fileListString = System.IO.File.ReadAllText("fileList.txt");
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("error:" + e.Message);
+            }
+            _fileList = fileListString
+                .Split(new[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => new KeyValuePair<string, int>(x, 0))
+                .ToDictionary(x => x.Key, x => x.Value);
         }
     }
 }
